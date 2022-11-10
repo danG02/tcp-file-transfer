3955a267af96edfc5af5448fc0ff2fa693d35068
Ibrahim Pulatov <ibrohimbek1999@gmail.com>
2022-11-09 21:40:46 -0500

    list of files

--- ibra.py
+++ ibra.py
@@ -1,6 +1,19 @@
 import os
+from tkinter import *
 
-path = ("C:\\Users\\ibroh\\OneDrive\\Documents\\GitHub\\tcp-file-transfer\\files")
-arr = os.listdir(path)
-for i in arr:
-    print(i, end = '\n')
+
+connected = Tk()
+connected.title("Server")
+connected.geometry("200x75")
+def applytoLabel():
+    path = ("..\\tcp-file-transfer\\files")
+    arr = os.listdir(path)
+    n = len(arr)
+    element = ''
+    for i in range(n):
+        element = element + arr[i] + '\n'
+    return element
+
+l = Label(connected, text=applytoLabel())
+l.grid(row = 1, column = 3)
+connected.mainloop()
