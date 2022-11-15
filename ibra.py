e4a79bd231f459454776f4b30ac1baa545a989e0
Ibrahim Pulatov <ibrohimbek1999@gmail.com>
2022-11-15 18:58:06 -0500

    look the ibra py

--- ibra.py
+++ ibra.py
@@ -1,19 +1,30 @@
 import os
 from tkinter import *
-
+import tkinter.messagebox as box
 
 connected = Tk()
 connected.title("Server")
-connected.geometry("200x75")
-def applytoLabel():
-    path = ("..\\tcp-file-transfer\\files")
-    arr = os.listdir(path)
-    n = len(arr)
-    element = ''
-    for i in range(n):
-        element = element + arr[i] + '\n'
-    return element
+connected.geometry("500x200")
+def downloadFile():
+    window = Tk()
+    window.title('Downloads')
+    frame = Frame(window)
+    myList = os.listdir(".\\files")
+    myListBox = Listbox(window)
+    for file in myList:
+        myListBox.insert(END, file)
+    myListBox.pack()
 
-l = Label(connected, text=applytoLabel())
-l.grid(row = 1, column = 3)
+    def dialog():
+        box.showinfo('Selection', 'Your Choice: ' + \
+                     myListBox.get(myListBox.curselection()) + ' was downloaded successfully')
+
+    btn = Button(frame, text='Download File', command=dialog)
+    btn.pack(side=RIGHT, padx=5)
+    myListBox.pack(side=LEFT)
+    frame.pack(padx=30, pady=30)
+
+b2 = Button(connected, text="Download", command=downloadFile)
+b2.grid(row=2, column=0)
+
 connected.mainloop()
